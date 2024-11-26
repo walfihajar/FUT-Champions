c446c2b4e88c784c85cf544be501ec7f9a39b235
Hajar Walfi <you@example.com>
2024-11-26 12:04:43 +0100

    new modifications

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Start Modal Manipulation//////////////////////////////////////////////////////////////////
 const add = document.getElementById("new"); 
 const modal = document.getElementById("modal");
 const cancel = document.getElementById("cancel");
@@ -16,3 +17,20 @@ cancel.addEventListener("click", () => {
 save.addEventListener("click", (event) => {
    event.preventDefault();
 });
+//End Modal Manipulation//////////////////////////////////////////////////////////////////
+
+
+//Start Data Recuperation//////////////////////////////////////////////////////////////////
+const nom = document.getElementById("nom").value;
+const position = document.getElementById("position").value;
+const note = document.getElementById("note").value;
+const pac = document.getElementById("pac").value;
+const sho = document.getElementById("sho").value;
+const pas = document.getElementById("pas").value;
+const dri = document.getElementById("dri").value;
+const def = document.getElementById("def").value;
+const phy = document.getElementById("phy").value;
+const nationalite = document.getElementById("nationalite").value;
+
+
+//End Data Recuperation//////////////////////////////////////////////////////////////////
